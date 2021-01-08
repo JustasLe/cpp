1822cfa51f363dc85921216469d4eea7bc9c9f0a
Justas <61835480+JustasLe@users.noreply.github.com>
2021-01-08 14:28:58 +0200

    Add files via upload

---
+++ IT VBE(Lithuanian National Maturity Exam) programming tasks/2016_VBE_1.Kuprines.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+
+using namespace std;
+
+/// funkcija, kuri suranda sunkiausia kuprine
+int rastiSunkiausiosKuprinesMase() {
+    freopen("U1.txt", "r", stdin);
+    int ats = 0;
+    int kiekKupriniu;
+    cin >> kiekKupriniu;
+    for (int i = 0; i < kiekKupriniu; i++) {
+        int svoris;
+        cin >> svoris;
+        ats = max(ats, svoris);
+    }
+    return ats;
+}
+
+/// funkcija, kuri suranda kiek kupriniu du ir daugiau kartu lengvesnes uz sunkiausia
+int rastiKiekKupriniuDuIrDaugiauKartuLengvesnes(int sunkiausiosSvoris) {
+    ifstream cin("U1.txt");
+    int kiekKupriniu, ats = 0;
+    cin >> kiekKupriniu;
+    for (int i = 0; i < kiekKupriniu; i++) {
+        int svoris;
+        cin >> svoris;
+        /// nenaudojame dalybos !
+        if (svoris * 2 <= sunkiausiosSvoris) {
+            ats++;
+        }
+    }
+    return ats;
+}
+
+int main() {
+    /// pasakome, kad dirbsime su failu ir atliekame skaiciavimus bei isvedame rezultatus
+    freopen("U1rez.txt", "w", stdout);
+    int sunkiausiosSvoris = rastiSunkiausiosKuprinesMase();
+    cout << sunkiausiosSvoris << " " <<
+        rastiKiekKupriniuDuIrDaugiauKartuLengvesnes(sunkiausiosSvoris) << "\n";
+    return 0;
+}
